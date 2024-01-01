cba86026e38147053b66363db538235bc36d2432
Derfortam <sstredmi@gmail.com>
2024-01-01 15:25:31 +0300

    Change lesson22

--- exp2/js/js.js
+++
@@ -1,7 +0,0 @@
-console.log("ready");
-$(document).ready(function () {
-    $('.header__burger').click(function (even) {
-        $('.header__burger, .header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-});
